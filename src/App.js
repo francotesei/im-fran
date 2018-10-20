1a70477a8005f116c50ffdec5e8efca2ed1b7a72
francotesei <ftesei96@gmail.com>
2018-10-20 16:48:00 -0300

    -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import './glitch.css';
+import Presentation from './components/Presentation';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <div className="glitch" data-text="GLITCH">GLITCH</div>
+        <Presentation/>
       </div>
     );
   }
---
+++ src/components/Presentation.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../glitch.css';
+
+
+
+export default class Presentation extends Component {
+
+
+
+  render() {
+    return(
+      <div className="presentation">
+        <div className="jumbotron  ">
+          <div className="container vertical-center ">
+            <h3 className="display-4">
+              <div className="glitch" data-text="I'M FRAN">I'M FRAN</div>
+              </h3>
+            <p className="lead text-center">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+            </div>
+</div>
+    )
+  }
+}
