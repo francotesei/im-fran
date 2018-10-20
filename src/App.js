9f89eb802f645d891d59bfb69065f9ef6e47d052
francotesei <ftesei96@gmail.com>
2018-10-20 17:31:54 -0300

    added aboutme.Added font: Roboto Mono

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Presentation from './components/Presentation';
-
+import AboutMe from "./components/AboutMe";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Presentation/>
+        <AboutMe/>
       </div>
     );
   }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+
+
+export default class AboutMe extends Component {
+
+
+
+  render() {
+    return(
+      <div className="aboutme ">
+      <div className="container">
+      <div className="row">
+      <div className="col-md-12">
+      <div>
+      <h5 >ABOUT ME</h5>
+      </div>
+
+      </div>
+<div className="row">
+
+<div className="col-md-2">
+</div>
+<div className="col-md-10">
+Lorem ipsum dolor sit amet, in quodsi vulputate pro. Ius illum vocent mediocritatem an, cule dicta iriure at. Ubique maluisset vel te, his dico vituperata ut. Pro ei phaedrum maluisset. Ex audire suavitate has, ei quodsi tacimates sapientem sed, pri zril ubique ut. Te cule tation munere noluisse. Enim torquatos ne pri, eum mollis salutandi corrumpit et, fugit apeirian duo ad.
+
+</div>
+
+</div>
+        </div>
+        </div>
+        </div>
+    )
+  }
+}
