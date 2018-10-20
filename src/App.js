4303cb196227d02b28585cdce65e940eebb62fb5
francotesei <ftesei96@gmail.com>
2018-10-20 19:52:40 -0300

    -

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
       <div className="App">
         <Presentation/>
         <AboutMe/>
-        <Experience/>
         <Education/>
+        <Experience/>
       </div>
     );
   }
--- src/components/Education.js
+++ src/components/Education.js
@@ -17,12 +17,15 @@ export default class Education extends Component {
   <div className="row">
     <div className="col-md-2"></div>
     <div className="col-md-10">
+      <div className="card">
+        <div className="card-body">
       <p><strong>2017 - Present</strong></p>
       <p><b>Senior Developer - Redbee Studios</b></p>
       <p>Duis ipsum ligula, feugiat nec auctor vitae, porta sit amet urna. Praesent ex lorem, porta nec aliquet eu, ullamcorper eget nibh.</p>
     </div>
   </div>
-
+</div>
+</div>
 
 <br/> <br/>
 
@@ -30,11 +33,15 @@ export default class Education extends Component {
   <div className="row">
     <div className="col-md-2"></div>
     <div className="col-md-10">
+      <div className="card">
+        <div className="card-body">
       <p><strong>2017 - Present</strong></p>
       <p><b>Senior Developer - Redbee Studios</b></p>
       <p>Duis ipsum ligula, feugiat nec auctor vitae, porta sit amet urna. Praesent ex lorem, porta nec aliquet eu, ullamcorper eget nibh.</p>
     </div>
   </div>
+</div>
+</div>
 
 
     </div>
