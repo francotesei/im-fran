8e4f06b1c02f183e448b46a3bae21744a845b1ff
francotesei <ftesei96@gmail.com>
2018-10-20 17:55:39 -0300

    Added experience

---
+++ src/components/Experience.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Experience extends Component {
+
+  render(){
+    return(
+    <div className="experience">
+    <div className="container">
+    <div className="row">
+    <div className="col-md-12">
+    <div>
+    <h5 >EXPERIENCE</h5>
+    </div>
+    </div>
+  </div>
+
+
+
+    </div>
+    </div>
+    )
+  }
+}
